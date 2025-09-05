4b55203580a03452283600dfb457e1cf010eb783
nhanhkimson.biu@gmail.com <nhanhkimson.biu@gmail.com>
2025-09-05 18:42:56 +0700

    Initial Commit

---
+++ ABA BANK/ABA_BANKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABA_BANKApp.swift
+//  ABA BANK
+//
+//  Created by Apple on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ABA_BANKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ABA BANKTests/ABA_BANKTests.swift
@@ -0,0 +1,17 @@
+//
+//  ABA_BANKTests.swift
+//  ABA BANKTests
+//
+//  Created by Apple on 9/5/25.
+//
+
+import Testing
+@testable import ABA_BANK
+
+struct ABA_BANKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
